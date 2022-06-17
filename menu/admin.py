e70abb932ca549e164eaf894dcf573ee2515698c
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-06-17 13:25:17 +0100

    Feat: Add models to admin panel

---
+++ menu/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import MenuItem, Menu
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'menu_item_name',
+        'menu_item_type',
+        'menu_item_price'
+    )
+    list_filter = ('menu_item_type',)
+    
+    
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'menu_name',
+        'get_items'
+    )
+    list_filter = ('menu_name',)
+    
+    def get_items(self, obj):
+        return "\n, ".join([m.menu_item_name for m in obj.menu_items.all()])
