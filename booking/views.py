e63d37af398bf3d608ae31aa63ebae79a4075193
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-06-27 00:51:24 +0100

    Feat: Add search functionality on booking ref

--- booking/views.py
+++ booking/views.py
@@ -3,14 +3,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Booking, Table
 from .forms import BookingForm
 
+from django.db.models import Q
+
 class CreateBookingView(LoginRequiredMixin, generic.CreateView):
     """
-    View to render createbookings 
+    View to render createbookings
     and allow user to create a booking
     """
     form_class = BookingForm
     template_name = 'booking/booking.html'
-    success_url = "/booking/createbooking"
+    success_url = "/booking/managebookings/"
     model = Booking
 
     def form_valid(self, form):
@@ -23,19 +25,17 @@ class CreateBookingView(LoginRequiredMixin, generic.CreateView):
         ))
         bookings_on_requested_date = Booking.objects.filter(
             booking_date=date, booking_time=time)
-        
+
         for booking in bookings_on_requested_date:
             for table in tables_with_capacity:
                 if table.table_number == booking.booked_table.table_number:
                     tables_with_capacity.remove(table)
                     break
-        print(f"Available Tables with capacity: {tables_with_capacity}")
-        
+
         lowest_capacity_table = tables_with_capacity[0]
         for table in tables_with_capacity:
             if table.capacity < lowest_capacity_table.capacity:
                 lowest_capacity_table = table
-        print(f"lowest_capacity_table is : {lowest_capacity_table}")
         form.instance.booked_table = lowest_capacity_table
         return super(CreateBookingView, self).form_valid(form)
 
@@ -46,13 +46,17 @@ class BookingsList(LoginRequiredMixin, generic.ListView):
     """
     model = Booking
     template_name = 'booking/managebookings.html'
+
     def get_queryset(self):
-        if self.request.user.is_staff:
+        query = self.request.GET.get('q')
+        if query:
+            return Booking.objects.filter(id=query)
+        elif self.request.user.is_staff:
             return Booking.objects.all()
         else:
             return Booking.objects.filter(customer=self.request.user)
-        
-        
+
+
 class EditBookingView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     """
     A view to provide a Form to the user
@@ -60,23 +64,17 @@ class EditBookingView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateVie
     """
     form_class = BookingForm
     template_name = 'booking/edit_booking.html'
-    success_url = "/booking/managebookings"
+    success_url = "/booking/managebookings/"
     model = Booking
 
     def test_func(self):
-        if self.request.user.is_staff:
-            return True
-        else:
-            return self.request.user == self.get_object().customer
-        
-        
+        return self.request.user.is_staff
+
+
 class DeleteBookingView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     """ A view to delete an event """
     model = Booking
     success_url = "/booking/managebookings"
 
     def test_func(self):
-        if self.request.user.is_staff:
-            return True
-        else:
-            return self.request.user == self.get_object().customer
+        return self.request.user.is_staff
