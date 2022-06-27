b28cf09418ea02799b00a67223ad3c7190b57b56
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-06-27 20:07:50 +0100

    Fix: Update heroku url in allowed hosts to fix 400

--- sizzleandsteak/settings.py
+++ sizzleandsteak/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEVELOPMENT
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://sizzle-and-steak.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sizzle-and-steak.herokuapp.com']
 
 
 # Application definition
