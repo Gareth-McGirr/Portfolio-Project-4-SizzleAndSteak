6860e948e8b54618aa0ea3d0a0bbb2045e32a5a2
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2023-02-27 22:44:35 +0000

    update tests

--- home/tests.py
+++ home/tests.py
@@ -8,5 +8,5 @@ class TestViews(TestCase):
     def test_home_page(self):
         """ Test home page renders correct page """
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
         self.assertTemplateUsed(response, 'home/index.html')
